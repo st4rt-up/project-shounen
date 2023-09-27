2899afbda65b9206c0b9de1338002324d95f10e3
Kai <34955546+st4rt-up@users.noreply.github.com>
2023-09-27 00:36:19 -0400

    one before I go to bed
    I think the state manager works now

--- RunState.gd
+++ RunState.gd
@@ -2,8 +2,11 @@ extends Node
 
 var fsm: StateManager
 var input_vector
+@onready var _animated_sprite = $AnimatedSprite2D
 
 @export var speed = 300.0
+
+signal direction_changed
 
 func enter():
 	pass
@@ -15,8 +18,9 @@ func process(delta):
 	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	var directions_pressed = sqrt(input_vector.x * input_vector.x + input_vector.y * input_vector.y) > 0
 	
-	owner.velocity = input_vector * speed
+	_animated_sprite.play("run")
 	
+	owner.velocity = input_vector * speed
 	owner.move_and_slide()
 	
 	if not directions_pressed:

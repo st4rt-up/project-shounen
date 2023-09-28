1d676f4e7cc81054a703fd1e5efb9cff146e3cfe
Kai <34955546+st4rt-up@users.noreply.github.com>
2023-09-27 22:18:02 -0400

    cleaned up file structure

--- anim.gd
+++ anim.gd
@@ -1,10 +1,10 @@
-extends Node
+extends AnimatedSprite2D
 
-signal state_changed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- characters/growlithe/RunState.gd
+++ characters/growlithe/RunState.gd
@@ -2,8 +2,8 @@ extends Node
 
 var fsm: StateManager
 var input_vector
-@onready var _animated_sprite = $AnimatedSprite2D
 
+@onready var _animated_sprite = find_node("anim")
 @export var speed = 300.0
 
 signal direction_changed
@@ -18,7 +18,7 @@ func process(delta):
 	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	var directions_pressed = sqrt(input_vector.x * input_vector.x + input_vector.y * input_vector.y) > 0
 	
-	_animated_sprite.play("run")
+	$anim.play("run")
 	
 	owner.velocity = input_vector * speed
 	owner.move_and_slide()
